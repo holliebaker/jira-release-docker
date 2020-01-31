507e84c500801e231586e08adf1d08543577dde5
Hollie Baker <hollie.baker@mayden.co.uk>
2020-01-31 10:27:14 +0000

    Git pull if repo already exists

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-cd /var/www/html/
-git clone https://${GITHUB_ACCESS_TOKEN}@github.com/may-den/jira-release-manager
+set -eux
 
-cd jira-release-manager
+cd /var/www/html/
+if [[ -d jira-release-manager ]]
+then
+    cd jira-release-manager && git pull
+else
+    git clone https://${GITHUB_ACCESS_TOKEN}@github.com/may-den/jira-release-manager && \
+    cd jira-release-manager
+fi
+
 cp .env.example .env
 sed -i -e "s/##USERNAME##/${JIRA_USERNAME}/g" .env
 sed -i -e "s/##API_KEY##/${JIRA_API_KEY}/g" .env
